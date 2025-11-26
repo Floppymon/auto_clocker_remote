900defc1a059c6ae627e297d89da5df0daa22bf5
Floppymon <60203724+Floppymon@users.noreply.github.com>
2025-11-26 13:43:45 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
-//
-const CACHE_NAME = 'clocker-remote-v3'; // <--- Bumped version
+const CACHE_NAME = 'clocker-remote-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
@@ -17,16 +14,5 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
-
-// NEW: Delete old caches (Clean up v1 and v2)
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
   );
 });
